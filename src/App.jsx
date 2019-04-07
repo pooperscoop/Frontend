75a7c1a6c50b14d24b159d5ac6090a103c8b1076
Asim Zaidi <asimzaidih@gmail.com>
2019-04-06 20:01:54 -0700

    partum-react :celebration:

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import logo from "./logo.svg";
+import "./App.scss";
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.jsx</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = state => ({
+	...state
+});
+
+const mapDispatchToProps = dispatch => ({
+	// your dispatches go here.
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
